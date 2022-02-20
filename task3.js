57b6f33be616680ac5a66b56dc9f0a7755a039a4
mohan8398 <mohankumar9875mk@gmail.com>
2022-02-20 22:05:50 +0530

    added task3.js

---
+++ task3.js
@@ -0,0 +1,11 @@
+var items =document.getElementByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+//items.textContent='ADD ITEM';
+items.fonrweight='bold';
+items[2].backgroundColor="green";
+
+
+for(var i=0; i<items.length; i++){
+    items[i].getElementsByClassName.backgroundColor="#f4f4f4";
+}
