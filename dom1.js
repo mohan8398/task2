e62fe42d07b92324918c50854eb68d4216cfef22
mohan8398 <mohankumar9875mk@gmail.com>
2022-02-20 20:02:28 +0530

    added dom1.js

---
+++ dom1.js
@@ -0,0 +1,34 @@
+
+//console.dir(document);
+//console.log(document.domain);
+//console.log(document.URL);
+//.log(document.title);
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//console.log(document.links);
+//console.log(document.images);
+ //get element by id
+//console.log(document.getElementById('header-title'));
+//var headerTitle=document.getElementById('header-title');
+//.log(headerTitle);
+//headerTitle.textContent='Hello';
+//.innerText='Goodbye';
+//.log(headerTitle.textContent);
+//.log(headerTitle.innerText);
+//.style.borderBottom='solid 3px #000'
+
+//get element by class name
+var items =document.getElementByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+//items.textContent='ADD ITEM';
+items.fonrweight='bold';
+items[2].backgroundColor="green";
+
+
+for(var i=0; i<items.length; i++){
+    items[i].getElementsByClassName.backgroundColor="#f4f4f4";
+}
